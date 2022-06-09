51a4cff15b5d80429b2f61c8ef80ba7405f99357
Andrew <andrewgaity@yandex.by>
2022-06-09 15:16:25 +0300

    fix: "should set current book" test

--- src/store/reducers/tests/books_reducer.test.ts
+++ src/store/reducers/tests/books_reducer.test.ts
@@ -1,3 +1,4 @@
+import { FIRST_ARRAY_ITEM } from 'constants/constants'
 import { booksReducer, setCurrentBookAC } from 'store/reducers/books_reducer'
 import { getBooksTC } from 'store/thunks/books_thunks'
 import { BooksInitialStateType } from 'store/types/BooksInitialStateType'
@@ -5,38 +6,67 @@ import { BookType } from 'store/types/BookType'
 
 let initialState: BooksInitialStateType
 let newBooks: BookType[]
-const bookId: string = '1'
+const bookId: string = '3'
 
 beforeEach(() => {
   initialState = {
-    books: [],
+    books: [
+      {
+        uuid: '3',
+        title: 'software testing',
+        direction: {
+          uuid: '40',
+          name: 'SAGA',
+        },
+        author: {
+          uuid: '1',
+          full_name: 'BOB',
+        },
+        difficulty: 'middle',
+        edition_date: '2022-06-07T00:00:00Z',
+        description: 'very usefull ',
+        local_url: '/file/book?file=author(1)-title(software_testing).pdf&uuid=2',
+        language: 'rus',
+        tags: [
+          {
+            uuid: '1',
+            name: 'newbies',
+          },
+          {
+            uuid: '2',
+            name: 'newbies2',
+          },
+        ],
+        image_url: '/image/book?format=QVGA&uuid=2',
+      },
+    ],
     currentBook: {
-      uuid: '1',
-      title: 'software testing',
+      uuid: '',
+      title: '',
       direction: {
-        uuid: '40',
-        name: 'SAGA',
+        uuid: '',
+        name: '',
       },
       author: {
-        uuid: '1',
-        full_name: 'BOB',
+        uuid: '',
+        full_name: '',
       },
-      difficulty: 'middle',
-      edition_date: '2022-06-07T00:00:00Z',
-      description: 'very usefull ',
-      local_url: '/file/book?file=author(1)-title(software_testing).pdf&uuid=1',
-      language: 'rus',
+      difficulty: '',
+      edition_date: '',
+      description: '',
+      local_url: '',
+      language: '',
       tags: [
         {
-          uuid: '1',
-          name: 'newbies',
+          uuid: '',
+          name: '',
         },
         {
-          uuid: '2',
-          name: 'newbies2',
+          uuid: '',
+          name: '',
         },
       ],
-      image_url: '/image/book?format=QVGA&uuid=1',
+      image_url: '',
     },
   }
 
@@ -114,6 +144,6 @@ describe('books reducer', () => {
 
     const endState = booksReducer(initialState, action)
 
-    expect(endState.books).toStrictEqual(newBooks)
+    expect(endState.currentBook).toStrictEqual(initialState.books[FIRST_ARRAY_ITEM])
   })
 })
