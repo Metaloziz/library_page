3953113851a8cc0aede94778de091c37f0fa1dc9
Andrew <andrewgaity@yandex.by>
2022-06-14 15:40:24 +0300

    refactoring: remove .docx-file viewer

--- src/pages/CurrentArticle/ArticleBody/ArticleBody.tsx
+++ src/pages/CurrentArticle/ArticleBody/ArticleBody.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react'
 
-import DocViewer, { DocViewerRenderers } from 'react-doc-viewer'
-
 import style from './ArticleBody.module.scss'
 
 import { NavLinkComponent } from 'components'
@@ -14,30 +12,19 @@ import { convertTitle } from 'utils/convert_title'
 export const ArticleBody: FC<ArticleBodyPropsType> = ({
   article: { title, edition_date, image_url, description },
   isAdmin,
-}) => {
-  const docs = [
-    {
-      uri: 'http://localhost:1000/file/article?uuid=10&file=author(1)-title(article_three).docx',
-    },
-  ]
-
-  return (
-    <div className={style.container}>
-      <div className={style.header}>
-        <div className={style.date}>{convertDateView(edition_date)}</div>
-        <NavLinkComponent nameButton="на главную" path={Path.MAIN} />
-        {isAdmin && <NavLinkComponent nameButton="редактировать" path={Path.MAIN} />}
-        <div>
-          <h1>{convertTitle(title)}</h1>
-        </div>
-      </div>
-      <div className={style.body}>
-        <img alt="logo" src={BASE_URL + image_url} />
-        <div className={style.description}>{description}</div>
-        <div>
-          <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />
-        </div>
+}) => (
+  <div className={style.container}>
+    <div className={style.header}>
+      <div className={style.date}>{convertDateView(edition_date)}</div>
+      <NavLinkComponent nameButton="на главную" path={Path.MAIN} />
+      {isAdmin && <NavLinkComponent nameButton="редактировать" path={Path.MAIN} />}
+      <div>
+        <h1>{convertTitle(title)}</h1>
       </div>
     </div>
-  )
-}
+    <div className={style.body}>
+      <img alt="logo" src={BASE_URL + image_url} />
+      <div className={style.description}>{description}</div>
+    </div>
+  </div>
+)
